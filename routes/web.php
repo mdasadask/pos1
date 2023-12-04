d90b01b9d0f8f20cf9b50f4321023e9cbf7a5570
Asad <mdasadask@gmail.com>
2023-12-05 00:10:17 +0600

    Public main asset located at up and down

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,16 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Livewire\Home;
+use App\Livewire\About;
+use App\Livewire\Contact;
+use App\Livewire\Counter;
+ 
+Route::get('/', Home::class)->name('home');
+Route::get('/about', About::class)->name('about');
+Route::get('/contact', Contact::class)->name('contact');
+Route::get('/counter', Counter::class)->name('counter');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
